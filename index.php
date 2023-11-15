068fd6039ec9808e3d18fbe224533570b342b737
youngdgg <144021000+youngdgg@users.noreply.github.com>
2023-11-15 10:24:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik zit op het ";
+
+$schoolnaam = "Techniek College Rotterdam !";
+
+echo "$schoolnaam";
+$getal1 = "<br>15<br>";
+echo "$getal1";
+//wat is het nummer van de politie
+$getal2 = "112";
+echo "$getal2";  
+
+?>
+
+
+
+
